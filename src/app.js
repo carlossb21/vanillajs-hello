afdf137e2f16244d265b06f47d1a472f64f852b6
carlossb21 <carlosandresb21@gmail.com>
2025-02-14 00:42:35 +0000

    tarea excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let aleatorio = (vector) => {
+    let numero = Math.floor(Math.random() * vector.length);
+    return vector[numero];
+  }
+
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let txtExcusa = document.getElementById('excuse');
+  txtExcusa.innerHTML = aleatorio(who) + ' ' + aleatorio(action)+ ' ' + aleatorio(what) + ' '+ aleatorio(when);
+
+  
 };
